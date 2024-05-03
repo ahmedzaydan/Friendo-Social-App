9eb00371c19a519e77079511cef7427108987a5f
ahmedzaydan <ahmedzidanesyrian@gmail.com>
2024-05-03 19:44:39 +0300

    Delete old project

--- friendo/android/app/src/main/kotlin/com/example/friendo/MainActivity.kt
+++ friendo/android/app/src/main/kotlin/com/example/friendo/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.friendo
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- friendo/lib/main.dart
+++ friendo/lib/main.dart
@@ -1,91 +1,125 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:friendo/layout/cubit/friendo_cubit.dart';
-import 'package:friendo/layout/home_layout.dart';
-import 'package:friendo/modules/authentication/login_screen.dart';
-import 'package:friendo/shared/bloc_observer.dart';
-import 'package:friendo/shared/components/constants.dart';
-import 'package:friendo/shared/components/ui_widgets.dart';
-import 'package:friendo/shared/network/local/cache_controller.dart';
-import 'package:friendo/shared/network/remote/fcm_handler.dart';
-import 'package:friendo/shared/styles/themes.dart';
 
-import 'modules/authentication/cubit/auth_cubit.dart';
-import 'modules/chats/cubit/chats_cubit.dart';
-import 'modules/posts/cubit/post_cubit.dart';
-
-Future<void> firebaseMessageHandler(RemoteMessage message) async {
-  if (kDebugMode) print('background message ${message.data.toString()}');
-  UIWidgets.showCustomToast(
-    message: 'background message',
-    state: ToastStates.SUCCESS,
-  );
+void main() {
+  runApp(const MyApp());
 }
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Bloc.observer = MyBlocObserver();
-  FCMHandler.init();
-  await Firebase.initializeApp();
-  await CacheController.init();
-  //1
-  // var token = await FirebaseMessaging.instance.getToken();
-  // if (kDebugMode) print("token: $token");
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // FirebaseMessaging.onMessageOpenedApp.listen((event) {
-  //   if (kDebugMode) (event.data.toString());
-  //   UIWidgets.showCustomToast(
-  //       message: 'outside the app', state: ToastStates.SUCCESS);
-  // });
-  // deviceToken = await FirebaseMessaging.instance.getToken() as String;
-  // FirebaseMessaging.onBackgroundMessage(firebaseMessageHandler);
-  // FirebaseMessaging.onMessage.listen((event) {
-  //   // if (kDebugMode) print(event.data.toString());
-  //   UIWidgets.showCustomToast(message: 'in app', state: ToastStates.SUCCESS);
-  // });
-  Widget startWidget;
-  currentUId = await CacheController.getData(key: 'uid');
-  currentUId == null
-      ? startWidget = LoginScreen()
-      : startWidget = const HomeLayoutScreen();
-  runApp(FriendoApp(startWidget));
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class FriendoApp extends StatelessWidget {
-  final Widget startWidget;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  const FriendoApp(this.startWidget, {super.key});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AuthCubit()),
-        BlocProvider(
-          create: (context) => FriendoCubit()
-            ..getUserModel(
-              userId: currentUId!,
-              isCurrentUser: true,
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-        ),
-        BlocProvider(
-          create: (context) => PostCubit()..getPosts(),
-        ),
-        BlocProvider(
-          create: (context) => ChatsCubit(),
-        ),
-      ],
-      child: SafeArea(
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: startWidget,
-          // home:  Temp(),
-          theme: lightTheme(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
